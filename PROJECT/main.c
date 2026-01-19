743b289324cc1925bbda50626efff95bfd4211ee
Mateusz-German <germanmateusz@gmail.com>
2026-01-19 23:28:25 +0100

    Uzycie funkcji utworzStworzenie z stworzenie.c do tworzenia listy stworzen

--- PROJECT/main.c
+++ PROJECT/main.c
@@ -2,28 +2,29 @@
 #include "stworzenia.h"
 
 int main(void) {
-    Stworzenie s = {
-        "Smaug",
-        "Smok",
-        95,
-        10,
-        "2243-06-12",
-        NIEBEZPIECZNY
-    };
+Stworzenie* s1 = utworzStworzenie(
+    "Smaug",
+    "Smok",
+    95,
+    10,
+    "2243-06-12",
+    NIEBEZPIECZNY
+);
 
-    Stworzenie s2 = {
-        "Fenix",
-        "Feniks",
-        70,
-        4,
-        "2243-06-10",
-        STABILNY
-    };
-    s.next = &s2;
-    s2.next = NULL;
+Stworzenie* s2 = utworzStworzenie(
+    "Fenix",
+    "Feniks",
+    70,
+    4,
+    "2243-06-10",
+    STABILNY
+);
+
+    s1->next = s2;
+    s2->next = NULL;
 
 
-    Stworzenie* aktualne = &s;
+    Stworzenie* aktualne = s1;
 
     while (aktualne != NULL) {
         printStworzenie(aktualne);
